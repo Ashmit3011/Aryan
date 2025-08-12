1dcf8fcdb6ea3757fefd8ca5f130f8da3291bb81
Ash_881 <practical3007@gmail.com>
2025-08-12 08:20:00 +0530

    Update cafe.py

--- cafe-management-system1-main/cafemanage/cafe.py
+++ cafe-management-system1-main/cafemanage/cafe.py
@@ -221,7 +221,7 @@ def menu_management_page():
                     menu_data[item_type].append(new_item)
                     save_json(MENU_FILE, menu_data)
                     st.success(f"Added {item_name} to menu!")
-                    #st.experimental_rerun()
+                    #st.rerun()
                 else:
                     st.error("Please fill all fields.")
 
@@ -263,7 +263,7 @@ def menu_management_page():
                             })
                             save_json(MENU_FILE, menu_data)
                             st.success("Item updated.")
-                            st.experimental_rerun()
+                            st.rerun()
             with col2:
                 if st.form_submit_button("Delete Item"):
                     t = item["_type"]
@@ -638,7 +638,7 @@ def main():
         st.session_state['logged_in'] = False
         st.session_state['user'] = None
         st.session_state['cart'] = []
-        st.experimental_rerun()
+        st.rerun()
     elif choice == "Dashboard":
         dashboard_page()
     elif choice == "Menu Management":
@@ -669,5 +669,6 @@ if __name__ == '__main__':
     if 'discount' not in st.session_state:
         st.session_state['discount'] = 0.0
     main()
+
 
 
